3ccbfb5255cbe2d1fa69a7e8285223b0358f5344
mrakhmadjon <72850225+mrakhmadjon@users.noreply.github.com>
2021-06-15 08:13:55 +0300

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,40 @@
+
+let allMusic = [
+  {
+    name: "ringtone 1",
+    artist: "lorem",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "ringtone 2",
+    artist: "lorem",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "ringtone 3",
+    artist: "lorem",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "ringtone 4",
+    artist: "lorem ",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "ringtone 5",
+    artist: "lorem",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "ringtone 6",
+    artist: "lorem",
+    img: "music-6",
+    src: "music-6"
+  },
+
+];
